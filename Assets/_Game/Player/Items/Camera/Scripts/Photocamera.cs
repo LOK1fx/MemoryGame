bdf576dc4d701700ebc51004284851577b13c72f
Igor <lok1gameproject.suport@gmail.com>
2024-03-19 20:39:58 +0900

    Added layer OnlyCameraSee

---
+++ Assets/_Game/Player/Items/Camera/Scripts/Photocamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOK1game
+{
+    public class Photocamera : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
