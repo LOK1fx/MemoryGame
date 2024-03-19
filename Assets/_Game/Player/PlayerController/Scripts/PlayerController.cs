9bf10db569ff72bed37229d52c5132cb6326d942
Igor <lok1gameproject.suport@gmail.com>
2024-03-20 05:54:57 +0900

    Added pause screen

--- Assets/_Game/Player/PlayerController/Scripts/PlayerController.cs
+++ Assets/_Game/Player/PlayerController/Scripts/PlayerController.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+using System;
+
 namespace LOK1game
 {
     public class PlayerController : Controller
     {
+        public event Action OnEscapePressed;
+
+        private bool _inPauseMenu;
+
         protected override void Awake()
         {
             
@@ -9,7 +16,16 @@ namespace LOK1game
 
         protected override void Update()
         {
-            ControlledPawn?.OnInput(this);
+            if (_inPauseMenu == false)
+                ControlledPawn?.OnInput(this);
+
+            //Right alt for editor tests
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.RightAlt))
+            {
+                _inPauseMenu = !_inPauseMenu;
+
+                OnEscapePressed?.Invoke();
+            }
         }
     }
 }
