563c0abf36e6466b43e80d317576064e02261b72
Igor <lok1gameproject.suport@gmail.com>
2024-03-21 19:52:24 +0900

    Updated cutscene, labirint01

--- Assets/_Game/Scripts/Settings/Scripts/PostProcessingWebFix.cs
+++ Assets/_Game/Scripts/Settings/Scripts/PostProcessingWebFix.cs
@@ -10,6 +10,8 @@ namespace LOK1game
         private AmbientOcclusion _ao;
         private AutoExposure _autoExposure;
         private ScreenSpaceReflections _srp;
+        private DepthOfField _depthOfField;
+        private ChromaticAberration _aberration;
 
         private void Start()
         {
@@ -17,18 +19,27 @@ namespace LOK1game
             _volume.profile.TryGetSettings(out _ao);
             _volume.profile.TryGetSettings(out _autoExposure);
             _volume.profile.TryGetSettings(out _srp);
+            _volume.profile.TryGetSettings(out _depthOfField);
+
+            _volume.profile.TryGetSettings(out _aberration);
 
 #if UNITY_WEBGL
 
             _ao?.enabled.Override(false);
             _autoExposure?.enabled.Override(false);
             _srp?.enabled.Override(false);
+            _depthOfField?.enabled.Override(false);
+
+            _aberration.fastMode.Override(true);
 
 #else
 
             _ao?.enabled.Override(true);
             _autoExposure?.enabled.Override(true);
             _srp?.enabled.Override(true);
+            _depthOfField?.enabled.Override(true);
+
+            _aberration.fastMode.Override(false);
 
 #endif
         }
