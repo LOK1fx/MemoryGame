c46316d9aefc72540827da7f7485bc5f8047fea4
Igor <lok1gameproject.suport@gmail.com>
2024-03-23 20:44:59 +0900

    Start working on main menu panels

---
+++ Assets/_Game/Utility/Scripts/CanvasGroupFade.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+
+namespace LOK1game.UI
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupFade : MonoBehaviour
+    {
+        public float TargetAlpha;
+
+        public bool IsShowing { get; private set; } = false;
+
+        [SerializeField] [Range(0f, 1f)] private float _startAlpha;
+        [SerializeField] private float _fadeSpeed;
+
+        private CanvasGroup _canvas;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<CanvasGroup>();
+            _canvas.alpha = _startAlpha;
+            TargetAlpha = _startAlpha;
+
+            if(_startAlpha == 0)
+            {
+                Hide();
+            }
+        }
+
+        private void Update()
+        {
+            _canvas.alpha = Mathf.Lerp(_canvas.alpha, TargetAlpha, _fadeSpeed * Time.deltaTime);
+        }
+
+        public void SetAlpha(float alpha)
+        {
+            alpha = Mathf.Clamp01(alpha);
+
+            if (alpha == 0)
+            {
+                Hide();
+            }
+            else if (alpha == 1)
+            {
+                Show();
+            }
+
+            TargetAlpha = alpha;
+        }
+
+        public void Show()
+        {
+            TargetAlpha = 1f;
+
+            _canvas.interactable = true;
+            _canvas.blocksRaycasts = true;
+
+            IsShowing = true;
+        }
+
+        public void InstaShow()
+        {
+            TargetAlpha = 1f;
+            _canvas.alpha = 1f;
+
+            _canvas.interactable = true;
+            _canvas.blocksRaycasts = true;
+
+            IsShowing = true;
+        }
+
+        public void Hide()
+        {
+            TargetAlpha = 0f;
+
+            _canvas.interactable = false;
+            _canvas.blocksRaycasts = false;
+
+            IsShowing = false;
+        }
+
+        public void InstaHide()
+        {
+            TargetAlpha = 0f;
+            _canvas.alpha = 0f;
+
+            _canvas.interactable = false;
+            _canvas.blocksRaycasts = false;
+
+            IsShowing = false;
+        }
+    }
+}
