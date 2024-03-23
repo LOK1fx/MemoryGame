4097c0c6d0b073ef91ce3cd1e3485ebe2f541480
Igor <lok1gameproject.suport@gmail.com>
2024-03-24 00:44:59 +0900

    Trying to fix

--- Assets/_Game/Levels/EntranceToMine/Scripts/Trolley.cs
+++ Assets/_Game/Levels/EntranceToMine/Scripts/Trolley.cs
@@ -1,7 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using LOK1game.Player;
 using UnityEngine;
-public class Trolley : MonoBehaviour
+
+namespace LOK1game
 {
-    
+    public class Trolley : MonoBehaviour, IInteractable
+    {
+        public void OnHighlight(bool isActive)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInteract(Player.Player sender)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetTooltip()
+        {
+            return "Press F to sit in the trolley";
+        }
+    }
 }
---
+++ Assets/_Game/Levels/EntranceToMine/Scripts/TrolleyPlayerAnimations.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LOK1game
+{
+    public class TrolleyPlayerAnimations : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
