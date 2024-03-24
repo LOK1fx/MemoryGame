251cfb5c2ac23a57c10129e720c4ac9201e48ff7
Midlmm <midl2525@mail.ru>
2024-03-25 02:05:58 +0300

    Start work Before Panel

--- Assets/_Game/Levels/EntranceToMine/Scripts/TriggerSceneText.cs
+++ Assets/_Game/Levels/EntranceToMine/Scripts/TriggerSceneText.cs
@@ -1,7 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class TriggerSceneText : MonoBehaviour
+using LOK1game.UI;
+namespace LOK1game
 {
-    
+    public class TriggerSceneText : MonoBehaviour
+    {
+        [SerializeField] private PlayerSpawner _playerSpawner;
+
+        private PlayerHUD _currentPlayerHUD;
+
+        private void Start()
+        {
+            _playerSpawner.OnSpawnedPlayerHud += SetPlayerHUD;
+        }
+
+        private void SetPlayerHUD(IPlayerHud hud)
+        {
+            _currentPlayerHUD = (PlayerHUD)hud;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<Player.Player>(out var player))
+            {
+                
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _playerSpawner.OnSpawnedPlayerHud -= SetPlayerHUD;
+        }
+    }
 }
